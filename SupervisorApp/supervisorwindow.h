a2662a381580cc74a0873006761cf59860e585b7
thehaipe <thehaipe2007@gmail.com>
2025-12-14 22:02:08 +0200

    add functions tamplates

--- SupervisorApp/supervisorwindow.h
+++ SupervisorApp/supervisorwindow.h
@@ -2,11 +2,16 @@
 #define SUPERVISORWINDOW_H
 
 #include <QMainWindow>
+#include <QSharedMemory>
+#include <QSystemSemaphore>
+#include <QTimer>
+#include <QMessageBox>
+#include <QListWidgetItem>
+
+#include "../Common/data_structures.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class SupervisorWindow;
-}
+namespace Ui { class SupervisorWindow; }
 QT_END_NAMESPACE
 
 class SupervisorWindow : public QMainWindow
@@ -17,7 +22,27 @@ public:
     SupervisorWindow(QWidget *parent = nullptr);
     ~SupervisorWindow();
 
+private slots:
+    void onUpdateTimerTick();  // Оновлення GUI
+    void onGameTimerTick();    // Відлік 3-х хвилин
+    void on_btnFinish_clicked(); // Кнопка завершення
+
 private:
     Ui::SupervisorWindow *ui;
+
+    // --- IPC ---
+    QSharedMemory *sharedMem;
+    QSystemSemaphore *semaphore;
+
+    // --- Логіка часу ---
+    QTimer *updateTimer;      // Опитування пам'яті
+    QTimer *gameTimer;        // Секундомір
+    int timeRemaining;        // Секунди до кінця (180 с)
+
+    // --- Допоміжні ---
+    void initIPC();           // Створення ресурсів
+    void cleanupIPC();        // Очистка
+    void showResults();       // Підрахунок голосів
 };
-#endif // SUPERVISORWINDOW_H
+
+#endif
